c01782211da30cba916b5e869db200e9aa3ad85d
Michael Mosher <mmosher47@gmail.com>
2018-02-16 08:26:23 -0500

    some light cleanup

--- cmd/import/main.go
+++ cmd/import/main.go
@@ -12,35 +12,27 @@ func main() {
 	cfg := readConfig("wio.toml")
 	// connect to mysql
 	// connect to redis
-	// create http client?
 	client := &http.Client{
 		Timeout: time.Second * 10,
 	}
 
 	j := jira.New(cfg.Jira.Hostname, cfg.Jira.Username, cfg.Jira.Password, client)
 
-	userChan := make(chan string, 5)
-	issueChan := make(chan jira.Issue, 30)
-
-	userChan <- "michaelm"
-	go handleUsers(j, userChan, issueChan)
-	handleIssues(j, issueChan)
-
-	close(userChan)
+	importIssues(j)
 	// fetch worklogs -> save worklogs
 	// publish worklogs to redis
 }
 
-func handleUsers(j jira.Client, users chan string, issues chan jira.Issue) {
-	for u := range users {
-		j.Issues(u, issues)
-	}
-}
+func importIssues(j jira.Client /*, database //coming soon */) {
+	users := [1]string{"michaelm"} // this will come from DB soon
 
-func handleIssues(_ jira.Client, issues chan jira.Issue) {
-	for i := range issues {
+	issueChan := make(chan jira.Issue)
+	for _, u := range users {
+		go j.Issues(u, issueChan)
+	}
+
+	for i := range issueChan {
 		fmt.Println(i)
 		// save issue
-		// pass issue into getWorklog channel
 	}
 }
